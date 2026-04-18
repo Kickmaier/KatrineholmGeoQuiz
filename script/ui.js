5263cb8276d251d5ad000039c793153e4cec292e
Tobias Kickmaier <t.kickmaier@hotmail.com>
2026-04-18 15:57:00 +0200

    added support for more webbrowsers to use camera

--- script/ui.js
+++ script/ui.js
@@ -59,7 +59,7 @@ function startCamera()
     navigator.mediaDevices.getUserMedia
     ({
         video : {
-            facingMode : "enviorment"
+            facingMode : { ideal: "environment" }
         }
     })
 .then(function(stream)
